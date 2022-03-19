0e8ef3d67c22363b1d23951e325dc4e48208b2e6
Melvin <melvingit@gmail.com>
2022-03-19 02:31:33 -0700

    started on convert and evaluate

--- project2/src/Application/Calculator.java
+++ project2/src/Application/Calculator.java
@@ -1,7 +1,31 @@
 public class Calculator{
     public static void main(String args[]){
-        System.out.println("Hello World");
-
-        sys
+        //LinkedStack<String> test = new LinkedStack<String>();
+        //test.peek();
+        testing("blahblah");
     }
+
+    public static String testing(String x){
+        System.out.println(x);
+        return x;
+        //System.out.println(x);
+    }
+/*
+    public String convertToPostfix(String infix){
+        //Converts an infix expression to an equivalent postfix expression.
+        LinkedStack<String> operatorStack = new LinkedStack<String>();
+        String postfix;
+
+        for (int i = 0; i < infix.length();i++){
+            Character nextCharacter = infix.charAt(i);
+            switch (nextCharacter){
+                case 
+            }
+        }
+    }
+
+    
+    */
 }
+
+
