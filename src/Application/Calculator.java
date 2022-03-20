1ae1e04bb19f3f61f28d5a9f0a3472478c98ebd5
Nich Rosen <nicholasrosen234@gmail.com>
2022-03-20 13:39:47 -0700

    updated Calculator and CalculatorTest.java

--- src/Application/Calculator.java
+++ src/Application/Calculator.java
@@ -6,8 +6,12 @@ public class Calculator{
     public static int a = 2;
     public static void main(String args[]){
         String y = "a*b/(c-a)+d*e";
+        String q = "a/b*(c+(d-e))";
+        String m = "a-b+c";
         //should return ab*ca-/de*+
-        //System.out.println(convertToPostfix(y));
+        System.out.println(convertToPostfix(y));
+        System.out.println(convertToPostfix(q));
+        System.out.println(convertToPostfix(m));
 /*
         int a = 2;
         int b = 3;
@@ -15,7 +19,7 @@ public class Calculator{
         int d = 5;
         int e = 6; */
         
-        System.out.println(evaluatePostfix(convertToPostfix(y)));
+        //System.out.println(evaluatePostfix(convertToPostfix(y)));
     }
 /*
     public static String testing(String x){
@@ -33,7 +37,7 @@ public class Calculator{
             Character nextCharacter = infix.charAt(i);
             if (Character.isLetter(nextCharacter)){
                 postfix += Character.toString(nextCharacter);
-                System.out.println("it is a letter");
+                //System.out.println("it is a letter");
             }
             else if (nextCharacter.equals('^')){
                 operatorStack.push(nextCharacter);
@@ -41,7 +45,7 @@ public class Calculator{
 
             
             else if(nextCharacter.equals('+') || nextCharacter.equals('-') || nextCharacter.equals('*') || nextCharacter.equals('/')){
-                System.out.println("it is an operator");
+                //System.out.println("it is an operator");
                 while (!operatorStack.isEmpty() && getPrecedence(nextCharacter) <= getPrecedence(operatorStack.peek())){ // (see getPrecedence) user input gets sanitized here. while loop will not execute if nextCharacter is not +, -, *, /, ^
                     postfix += Character.toString(operatorStack.peek());
                     operatorStack.pop();
--- tests/CalculatorTest.java
+++ tests/CalculatorTest.java
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.Test;
+//import org.junit.*;
+import static org.junit.Assert.AssertEquals;
 
 public class CalculatorTest{
+    //Calculator tester = new Calculator();
     @Test
     void testconvertToPostfix(){
-        assertEquals("ab*ca-/de*+", Application.Calculator.evaluatePostfix("a*b/(c-a)+d*e"));
+        assertEquals("ab*ca-/de*+", Calculator.evaluatePostfix("a*b/(c-a)+d*e"));
     }
 }
