301e8bb536454e133a093ed0c2834cade66b0e0e
Melvin <melvingit@gmail.com>
2022-03-20 21:03:53 -0700

    added stuff to test

--- tests/CalculatorTest.java
+++ tests/CalculatorTest.java
@@ -1,11 +1,23 @@
 import org.junit.jupiter.api.Test;
-//import org.junit.*;
+
+import Application.Calculator;
+
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest{
     //Calculator tester = new Calculator();
     @Test
     void testconvertToPostfix(){
-        assertEquals("ab*ca-/de*+", Application.Calculator.evaluatePostfix("a*b/(c-a)+d*e"));
+        String expected = "abca-/de+";
+        String actual = Calculator.convertToPostfix("ab/(c-a)+de");
+        assertEquals(expected, actual);
+
+
+    }
+    @Test
+    void testevaluatePostfix(){
+        int expected = 33;
+        int actual = Calculator.evaluatePostfix("2342-/56+");
+        assertEquals(expected, actual);
     }
 }
